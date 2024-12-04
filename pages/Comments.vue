cff42343e716bb0b22a911a825bcb6b751d84c97
AndreySamaev <andreysamaev@gmail.com>
2024-12-04 21:39:32 +0200

    Revert "lazy loading added"
    This reverts commit e29127a0e0ec1debe3a65c694501bbc92161a7d6.

--- pages/Comments.vue
+++ pages/Comments.vue
@@ -1,67 +1,18 @@
 <template>
-  <div ref="commentList" v-for="comment of visibleComments" :key="comment.id">
-    <div>{{ comment.id }} - {{ comment.body }}</div>
-  </div>
-  <div v-if="hasMoreComments" class="loading">Loading more comments...</div>
+  <Suspense>
+    <template #default>
+      <div v-for="comment of comments" :key="comment.id">
+        <div>{{ comment.id }} - {{ comment.body }}</div>
+      </div>
+    </template>
+    <template #fallback>
+      Loading comments...
+    </template>
+  </Suspense>
 </template>
 
 <script setup>
-import {ref, onMounted, onUnmounted} from 'vue';
- 
+import { useFetch } from '#app';
 
-const comments = ref([]); // Array to store fetched comments
-const visibleComments = ref([]); // Array for currently displayed comments
-const hasMoreComments = ref(true); // Flag to indicate availability of more comments
-const isLoading = ref(false); // Flag to prevent duplicate requests
-
-const fetchComments = async (startIndex = 0, limit = 50) => {
-  isLoading.value = true;
-  try {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/comments?_start=${startIndex}&_limit=${limit}`);
-    const newComments = await response.json();
-
-    // Update visibleComments directly with new comments
-    visibleComments.value = visibleComments.value.concat(newComments);
-
-    comments.value = comments.value.concat(newComments); // Update full comments list
-
-    if (newComments.length < limit) {
-      hasMoreComments.value = false;
-    }
-  } catch (error) {
-    console.error('Error fetching comments:', error);
-  } finally {
-    isLoading.value = false;
-  }
-};
-
-const handleScroll = () => {
-  if (isLoading.value || !hasMoreComments.value) return;
-
-  const {scrollTop, scrollHeight, clientHeight} = document.documentElement || document.body;
-  const isNearBottom = scrollTop + clientHeight >= scrollHeight - 100; // Threshold for near bottom
-
-  if (isNearBottom) {
-    fetchComments(comments.value.length); // Fetch more comments based on current length
-  }
-};
-
-onMounted(async () => {
-  await fetchComments();
-  window.addEventListener('scroll', handleScroll);
-});
-
-onUnmounted(() => {
-  // Clean up event listener (optional, if performance is critical)
-  window.removeEventListener('scroll', handleScroll);
-});
-
-
+const { data: comments} = await useFetch( 'https://jsonplaceholder.typicode.com/comments');
 </script>
-
-<style scoped>
-.loading {
-  text-align: center;
-  margin: 10px 0;
-}
-</style>
