a1f860c8d43971b02b2c001ee92f84daf3e6f50d
AndreySamaev <andreysamaev@gmail.com>
2025-02-20 08:39:49 +0200

    i frame added

--- pages/index.vue
+++ pages/index.vue
@@ -8,8 +8,15 @@
   <main>
     This an official widget from user-friendly yacht rental search engine that guarantees the best prices.
   </main>
+  <NuxtImg
+      quality="100"
+      loading="lazy"
+      height="245"
+      width="190"
+      placeholder
+      src="/1.webp"/>
 
-  <iframe src="http://localhost:3000/ru/frame/search?destinations=turkey" width="100%" height="800px" style="border: none;"></iframe>
+<!--  <iframe src="http://localhost:3000/ru/frame/search?id=92eccb92-3631-4aab-85b1-2ef89c489112&destinations=turkey,croatia" width="100%" height="800px" style="border: none;"></iframe>-->
 
 
 </div>
